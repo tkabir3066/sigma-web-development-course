2ed52ee4144ae83624031703d80ce7a4beb4e14a
Tutul Kabir <tutulkabir@gmail.com>
2023-11-27 18:37:48 +0530

    conditional statements if, else if, else

---
+++ 3.Javascript/56.if,else,else if lader/index.js
@@ -0,0 +1,51 @@
+// console.log("Hello World!!");
+/* 
+// let age = 45;
+let age = 5;
+
+if (age > 18) {
+  console.log("You can drive");
+} else {
+  console.log("You cannot drive");
+} */
+
+let age = 17;
+let grace = 2;
+
+console.log(age + grace);
+console.log(age - grace);
+console.log(age * grace);
+console.log(age / grace);
+console.log(age ** grace);
+console.log(age % grace);
+
+/*
+I am a 
+multiline 
+comment
+*/
+
+if (age == 18) {
+  console.log("You can drive");
+} else if (age == 0) {
+  console.log("Are you kidding?");
+} else if (age == 1) {
+  console.log("Are you again kidding?");
+} else {
+  console.log("You cannot drive");
+}
+
+a = 6;
+b = 8;
+let c = a > b ? a - b : b - a;
+
+/*
+translates to:
+if(a>b){
+    let c = a - b;
+}
+else {
+    let c = a - b;
+}
+
+*/

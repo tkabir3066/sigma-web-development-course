f56e7d9e6d8757e6155b3524d619e6c13ec483dc
Tutul Kabir <tutulkabir@gmail.com>
2023-11-28 10:21:15 +0530

    Exercise-9 Faulty Calculator

---
+++ 3.Javascript/59.Faulty Calculator(Exercise-9)/faulty-calculator.js
@@ -0,0 +1,31 @@
+/*
+  Create a faulty calculator using Javascript 
+  
+  This faulty calculator does following :
+
+  1. It takes two numbers as an input from the user
+  2. It performs as follows:
+
+  + -----> -
+  * -----> +
+  - -----> /
+  / -----> **
+  
+  it performs wrong operation 10% of the times
+  
+  */
+
+let randomNum = Math.random();
+
+let a = +prompt("Enter first number: ");
+let b = prompt("Enter operation: ");
+let c = +prompt("Enter second number: ");
+
+if (randomNum > 0.1) {
+  // perform correct calculation
+  console.log(`The result is ${eval(`${a} ${b} ${c}`)}`);
+} else {
+  // perform correct calculation
+  b = obj[b];
+  console.log(`The result is ${eval(`${a} ${b} ${c}`)}`);
+}

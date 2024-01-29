8fa5d564ec110054b503ee3a02467336ae9fa21b
Tutul Kabir <tutulkabir@gmail.com>
2024-01-29 20:01:03 +0530

    mongoose using with express

---
+++ 3.Javascript/96.Installing Mongoose & Using it with Express/models/Todo.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+// const TodoSchema = new mongoose.Schema({
+//   title: String,
+//   desc: String,
+//   isDone: Boolean,
+// });
+
+const TodoSchema = new mongoose.Schema({
+  title: { type: String, required: true, default: "Hey" },
+  desc: String,
+  isDone: Boolean,
+  days: Number,
+});
+export const Todo = mongoose.model("Todo", TodoSchema);
